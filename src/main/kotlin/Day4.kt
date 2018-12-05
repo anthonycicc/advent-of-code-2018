a08206e664fbc4e317f412eefecb1e2fa5ad3df5
acicchetti <acicchetti@constantcontact.com>
2018-12-05 14:17:41 -0500

    Better deserialization of Day 4

--- src/main/kotlin/Day4.kt
+++ src/main/kotlin/Day4.kt
@@ -4,16 +4,16 @@ import java.time.format.DateTimeFormatter
 class Day4(inputList: List<String>) {
     private val dataList: List<Entry>
     init {
-        this.dataList = inputList
-            .map { entryString -> entryString.toEntry() }
-            .sortedBy { it.dateTime }
-            .apply {
-                for (date in this.groupBy { it -> it.dateTime.dayOfYear }) {
-                    for (entry in date.value) {
-                        entry.guardNum = date.value[0].guardNum
-                    }
+        val tempList = mutableListOf<Entry>()
+        var guardNum = 0
+
+        for (entry in inputList.sorted()) {
+            if (entry.find { it == '#' } != null) {
+                guardNum = entry.between("#", " ").toInt()
             }
-            }
+            tempList.add(Entry(entry, guardNum))
+        }
+        this.dataList = tempList.toList()
     }
 
     fun guardAsleepLongest(): Int {
@@ -22,10 +22,9 @@ class Day4(inputList: List<String>) {
     }
 }
 
-class Entry(inp: String) {
+class Entry(inp: String, val guardNum: Int) {
     val dateTime: LocalDateTime
     val type: EventType
-    var guardNum: Int
 
     init {
         val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
@@ -35,7 +34,6 @@ class Entry(inp: String) {
             "wakes up" in inp -> Awoke
             else -> NewGuard
         }
-        this.guardNum = if (this.type == NewGuard ) { inp.between("#", " ").toInt() } else { 0 }
     }
 
     override fun toString(): String {
@@ -49,10 +47,6 @@ object NewGuard : EventType()
 object FellAsleep : EventType()
 object Awoke : EventType()
 
-
-fun String.toEntry(): Entry {
-    return Entry(this)
-}
 fun String.between(left: String, right: String): String {
     return this.substring(this.indexOf(left) + 1, this.indexOf(right, this.indexOf(left)))
 }
